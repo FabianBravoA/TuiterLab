c66b2a264efacca3a45e7f0f40253988f2fa4cfb
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2017-11-02 17:36:52 -0300

    Validación de input para el tuiter chileno

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 window.onload = function() {
-
+    /*Esto es porque soy flojo, getElements... entrega arreglo y 
+    solo queremos el primero de sus elementos que retorna porque 
+    sabemos que existe al menos uno*/
+    var sendButton = document.getElementsByName("enviar")[0];
+    var thinkInput = document.getElementsByName("loQuePenso")[0];
+    sendButton.onclick = function() {
+        if (thinkInput.value == "") {
+            alert("tení k poner algo oe!");
+        }
+    }
 }
