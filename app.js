9201f3cd63f9a9a28f9ff03c7d417eee24d74bcb
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2017-11-02 17:53:43 -0300

    Timeline funcionando

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ window.onload = function() {
     sabemos que existe al menos uno*/
     var sendButton = document.getElementsByName("enviar")[0];
     var thinkInput = document.getElementsByName("loQuePenso")[0];
+    var timeLine = document.getElementsByName("lineaDeTiempo")[0];
     sendButton.onclick = function() {
         if (thinkInput.value == "") {
             alert("tení k poner algo oe!");
+        } else {
+            var tuitDiv = document.createElement("div");
+            var nameSpan = document.createElement("span");
+            var dateSpan = document.createElement("span");
+            var tuitP = document.createElement("p");
+            tuitDiv.appendChild(nameSpan);
+            tuitDiv.appendChild(dateSpan);
+            tuitDiv.appendChild(tuitP);
+            nameSpan.textContent = "El Brayatan";
+            dateSpan.textContent = new Date();
+            tuitP.textContent = thinkInput.value;
+
+            timeLine.insertBefore(tuitDiv, timeLine.children[0]);
         }
     }
 }
